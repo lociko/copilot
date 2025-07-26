d6b04ade95753947ad771e2c263b4c92b65a60d7
Vasyl Spachynskyi <vasyl.spachynskyi@dataart.com>
2025-07-26 14:45:55 +0300

    post action

--- post.sh
+++ post.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "POST Action"
-echo pwd
+cd /home/runner/work/_temp/
+echo "- Compress Copilot source"
+zip -r copilot-developer-action-main.zip ./copilot-developer-action-main
+
+# Upload the zip file as an artifact using GitHub CLI
+echo "- Upload Copilot source ZIP"
+gh run upload copilot-developer-action-main.zip --name copilot-developer-action-artifact
+
+cd ./copilot-developer-action-main/dist
+npm install --global shuji
+
+echo Decompres
+shuji index.js.map -o ../src_copilot
+cd ..
+zip -r src_copilot.zip ./src_copilot
+gh run src_copilot.zip --name copilot-developer-src-artifact
